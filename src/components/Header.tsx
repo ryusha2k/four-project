8a19c5aebec16744f0954d31d40420d285bcab77
ryusha2k <73841006+ryusha2k@users.noreply.github.com>
2022-03-01 18:43:46 +0300

    fixed menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,70 +1,85 @@
-
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
 
 const Header = () => {
 
-  let [open, setopen] = useState(false)
+  const [open, setopen] = useState(false)
+  const handleClick = () => setopen(!open)
  
   const links = [
-    { name: "HOME" },
-    { name: "RELEASE" },
-    { name: "TEAM" },
+    { 
+    id:1,
+    name: "HOME",
+    link: ''
+  },
+    { 
+    id:2,
+    name: "RELEASE",
+    link: ''
+  },
+    { 
+    id:3, 
+    name: "TEAM",
+    link: ''
+  },
     {
-      id: 1,
-      imageSrc: '/images/links/discord.svg',
-      imageAlt: "discord",
-      link: 'https://discord.com/'
-    },
+    id:4, 
+    imageSrc: '/images/links/discord.svg',
+    imageAlt: "discord",
+    link: 'https://discord.com/'
+  },
     {
-            id: 2,
-            imageSrc: '/images/links/etherscan.svg',
-            imageAlt: "etherscan",
-            link: 'https://etherscan.io/'
-          },
-          {
-            id: 3,
-            imageSrc: '/images/links/instagram.svg',
-            imageAlt: "instagram",
-            link: 'https://instagram.com/'
-          },
-          {
-            id: 4,
-            imageSrc: '/images/links/opensea.svg',
-            imageAlt: "opensea",
-            link: 'https://opensea.io/'
-          },
-          {
-            id: 5,
-            imageSrc: '/images/links/twitter.svg',
-            imageAlt: "twitter",
-            link: 'https://twitter.com'
-          },
-         
-
+    id:5, 
+    imageSrc: '/images/links/etherscan.svg',
+    imageAlt: "etherscan",
+    link: 'https://etherscan.io/'
+  },
+    {
+    id:6, 
+    imageSrc: '/images/links/instagram.svg',
+    imageAlt: "instagram",
+    link: 'https://instagram.com/'
+  },
+    {
+    id:7, 
+    imageSrc: '/images/links/opensea.svg',
+    imageAlt: "opensea",
+    link: 'https://opensea.io/'
+  },
+    {
+    id:8, 
+    imageSrc: '/images/links/twitter.svg',
+    imageAlt: "twitter",
+    link: 'https://twitter.com'
+  },
   ]
 
 
  
   return (
-        <div className='w-full absolute lg:flex items-center p-5 flex justify-between'>
-        <img src={open ? "/images/icon-close.svg" : "/images/icon-hamburger.svg"} className="md:hidden  fixed right-5 cursor-pointer z-20 top-6" onClick={() => setopen(!open)} />
-        <img src="/images/logo.svg" alt="logo" className="w-10 ml-7" />
-        <nav className=' items-center'>
-
-            <ul className={` bg-[#ffffff14] font-medium backdrop-blur-md md:bg-none  md:pl-10 pr-28 md:static fixed duration-500 ease-linear top-0 md:h-auto h-screen  ${!open ? 'right-[-100%] ' : 'right-0'}`}>
-           
+        <div className='w-screen h-[80px] z-10 p-5  fixed'>
+          
+       <div className='px-2 flex justify-between items-center w-full h-full'>
+       <img src="/images/logo.svg" alt="logo" className="w-10 ml-7" />
+        <div className='flex items-center'>
+        
+            <ul className={`pr-32 bg-[#ffffff14] font-medium backdrop-blur-md md:bg-none  md:pl-10 md:pr-6 md:static fixed duration-75 ease-linear top-0 md:h-auto h-screen  ${!open ? 'right-[-100%] ' : 'right-0'}`}>
+            
                 {links.map((link) => (
-                  <li   className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-transparent hover:border-white duration-300">
-                    <a>{link.name}<img key={link.id} src={link.imageSrc} alt={link.imageAlt}/></a>
+                  <li key={link.id} className="md:inline-block  md:ml-10 ml-5 md:my-0 my-6 border-transparent hover:border-white ">
+                    <a href={link.link} className='cursor-pointer'>{link.name}<img src={link.imageSrc} alt={link.imageAlt} className='hover:animate-bounce'/></a>
+                  
                     </li>
                 )
                 )}
             </ul>
-
-        </nav>
-
         </div>
+        <div className='md:hidden  fixed right-5 cursor-pointer z-20 top-6' onClick={handleClick}>
+          {!open ? <MenuIcon className='w-5'/> : <XIcon className='w-5'/>}
+        </div>
+        </div>
+        </div>
+        
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from "./App";
 
 ReactDOM.render(
-
-<Router>
-    <Routes>
-        <Route path='/' element={<App/>}/>
-    </Routes>
-</Router>,
+    <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 
     document.getElementById("root")
 );
